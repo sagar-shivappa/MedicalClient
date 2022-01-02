01d1bd274b5fe3e809a9899acf845c9d3b36f55a
sagar-shivappa <shivappasagar@gmail.com>
2022-01-02 10:36:45 +0530

    table for indented stock

--- src/app/Stocks/indented-stocks/indented-stocks.component.ts
+++ src/app/Stocks/indented-stocks/indented-stocks.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-indented-stocks',
   templateUrl: './indented-stocks.component.html',
-  styleUrls: ['./indented-stocks.component.css']
+  styleUrls: ['./indented-stocks.component.css'],
 })
 export class IndentedStocksComponent implements OnInit {
-
-  constructor() { }
+  @Input() indStockList: any;
+  newList: any;
+  constructor() {}
 
   ngOnInit(): void {
+    this.newList = this.indStockList.filter((item: any) => item.quantity < 20);
+    console.log('intdented', this.newList);
   }
-
 }
