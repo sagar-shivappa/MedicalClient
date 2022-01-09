89c3073dd4ed53485dabbb61c29dc8827448610b
sagar-shivappa <shivappasagar@gmail.com>
2022-01-09 13:50:32 +0530

    css changes in intended stocks

--- src/app/Stocks/indented-stocks/indented-stocks.component.ts
+++ src/app/Stocks/indented-stocks/indented-stocks.component.ts
@@ -1,4 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { AddItemPopComponent } from '../add-item-pop/add-item-pop.component';
 
 @Component({
   selector: 'app-indented-stocks',
@@ -8,7 +14,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class IndentedStocksComponent implements OnInit {
   @Input() indStockList: any;
   newList: any;
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.newList = this.indStockList.filter((item: any) => item.quantity < 20);
@@ -23,5 +29,14 @@ export class IndentedStocksComponent implements OnInit {
     let a = this.newList.filter((item: any) => item.id == value);
     // console.log(this.newList.indexOf(a[0]), 1);
     this.newList.splice(this.newList.indexOf(a[0]), 1);
+  }
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddItemPopComponent, {
+      width: '250px',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
   }
 }
