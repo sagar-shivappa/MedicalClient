556ebe694bdb15ba7514f6ce087733d7f30be754
sagar-shivappa <shivappasagar@gmail.com>
2022-01-29 16:09:34 +0530

    heruko dep

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/TEST-APP"));
+app.use(express.static("./dist/test-app"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/TEST-APP/" })
+  res.sendFile("index.html", { root: "dist/test-app/" })
 );
 
 // Start the app by listening on the default Heroku port
