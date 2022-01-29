357cd65e727ce9235f066197c58e9aa47c23540e
sagar-shivappa <shivappasagar@gmail.com>
2022-01-29 13:12:26 +0530

    register Intiated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BaseLayoutComponent } from './Home/base-layout/base-layout.component';
@@ -50,7 +51,7 @@ import { RegisterUserComponent } from './RegisterUser/register-user/register-use
     BrowserModule,
     AppRoutingModule,
     MatFormFieldModule,
-
+    ReactiveFormsModule,
     FormsModule,
     CommonModule,
     MatTabsModule,
