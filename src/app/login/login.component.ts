1728139048c1051bdf16ad74915b5847754cff1b
sagar-shivappa <shivappasagar@gmail.com>
2023-10-17 21:31:57 +0530

    Login Fix phase 1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,63 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {
-  MatDialog,
-  MatDialogRef,
-  MAT_DIALOG_DATA,
-} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { RegisterUserComponent } from '../RegisterUser/register-user/register-user.component';
 import { LoginService } from './login.service';
 import { SharedService } from '../shared.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  public login = {
-    phoneNumber: '',
-    passWord: '',
-  };
+  login: FormGroup;
   errorMessage: any;
-  password: any;
   loader: boolean = false;
+
   constructor(
     private router: Router,
     public dialog: MatDialog,
     public loginService: LoginService,
-    public sharedService: SharedService
+    public sharedService: SharedService,
+    public fb: FormBuilder
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.login = this.fb.group({
+      phoneNumber: ['', Validators.required],
+      passWord: ['', Validators.required],
+    });
+  }
 
   submitHandler() {
-    this.loader = true;
-    this.errorMessage = '';
-    this.loginService.loginPost(this.login).subscribe((data: any) => {
-      if (data.message === 'LogIn Success') {
-        this.loader = false;
-        this.sharedService.getLoggedInUser(data);
-        this.router.navigate(['/admin', data.nickName]);
-      } else {
-        this.login = {
-          phoneNumber: '',
-          passWord: '',
-        };
-        this.loader = false;
-        this.errorMessage = data.message;
-        this.router.navigate(['/login']);
-      }
-    });
-
-    // if (this.login.userName == 'sagar' && this.login.password == 'sagar') {
-    //   this.router.navigate(['/admin', this.login.userName]);
-    // } else {
-    //   this.login = {
-    //     userName: '',
-    //     password: '',
-    //   };
-    //   this.router.navigate(['/login']);
-    // }
+    if (this.login.valid) {
+      this.loader = true;
+      this.errorMessage = '';
+      this.loginService.loginPost(this.login.value).subscribe((data: any) => {
+        if (data.status === 200) {
+          this.loader = false;
+          this.sharedService.getLoggedInUser(data);
+          this.router.navigate(['/admin', data.nickName]);
+        } else {
+          this.loader = false;
+          this.errorMessage = data.message;
+          this.router.navigate(['/login']);
+        }
+      });
+    }
   }
   openDialog(): void {
     const dialogRef = this.dialog.open(RegisterUserComponent, {
